36eaa5ff83084c9b7334784978e00767474bdcd1
ChetanBrightly <chetan.sengar@si-eam.com>
2024-11-04 15:21:44 +0530

    updating the code

---
+++ pocapplication/runAppLocal.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Define a function to check the last command's result
+check_status() {
+  if [ $? -ne 0 ]; then
+    echo "An error occurred. Stopping the script."
+    exit 1
+  fi
+}
+
+# Start the build process
+echo "Starting the build process..."
+./gradlew clean build
+check_status
+
+echo "Build successful. Starting Quarkus in local mode..."
+./gradlew quarkusDev
+check_status
+
+echo "Quarkus is running in local mode."
